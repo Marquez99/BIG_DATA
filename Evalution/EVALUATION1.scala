de5091baf3f37003026481cd31dca1b8fed8928a
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-09-28 19:13:57 -0700

    Upload the dataframe of NEtflix

--- Evalution/EVALUATION1.scala
+++ Evalution/EVALUATION1.scala
@@ -4,3 +4,10 @@
 /1.Comienza una simple sesión Spark. 
 import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
+
+
+//2.Cargue el archivo Netflix Stock CSV, haga que Spark infiera los tipos de datos. 
+val Dataf = spark.read.option("header","true").option("inferSchema","true").csv("Netflix_2011_2016.csv")
+
+//3. ¿Cuáles son los nombres de las columnas? 
+Dataf.columns
