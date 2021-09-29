3169615544bb97af92c800fbe2dc333d10d1af22
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-09-28 19:13:27 -0700

    Start of SPark session on evaluation

--- Evalution/EVALUATION1.scala
+++ Evalution/EVALUATION1.scala
@@ -1,3 +1,6 @@
 //EVALUACION NUMERO #1 DATOS MASIVOS
 //MARQUEZ ENRIQUEZ KEVIN EDWIN 17212923
 
+/1.Comienza una simple sesión Spark. 
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
