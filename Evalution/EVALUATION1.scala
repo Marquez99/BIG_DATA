699633f57e02adc48d76abcd4867538f4bd8208e
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-09-28 19:14:30 -0700

    Evaluation 1 SPrint

--- Evalution/EVALUATION1.scala
+++ Evalution/EVALUATION1.scala
@@ -11,3 +11,21 @@ val Dataf = spark.read.option("header","true").option("inferSchema","true").csv(
 
 //3. ¿Cuáles son los nombres de las columnas? 
 Dataf.columns
+
+
+//4. ¿Cómo es el esquema? 
+
+Dataf.printSchema()
+
+//5. Imprime las primeras 5 columnas. 
+
+Dataf.head(5)
+
+//6. Usa describe () para aprender sobre el DataFrame. 
+
+Dataf.describe().show()
+
+//7. Crea un nuevo dataframe con una columna nueva llamada “HV Ratio” que es la relación que  esixte entre el precio de la columna “High” frente a la columna “Volumen” de acciones  negociadas por un día. Hint es una operación 
+
+val Dataf2 = Dataf.withColumn("HV Ratio",Dataf("High")/Dataf("Volume"))
+
