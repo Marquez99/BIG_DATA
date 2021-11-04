77b296e3f175586594d7e4afec8be0fed3f9b297
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-03 20:41:23 -0700

    Making Predictions of Practice 1

--- Practices/Practice1 Decision tree classifier.scala
+++ Practices/Practice1 Decision tree classifier.scala
@@ -7,3 +7,40 @@ import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 // Load the data stored in LIBSVM format as a DataFrame.
 val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
+
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels in index.
+val labelIndexer = new StringIndexer()
+  .setInputCol("label")
+  .setOutputCol("indexedLabel")
+  .fit(data)
+// Automatically identify categorical features, and index them.
+val featureIndexer = new VectorIndexer()
+  .setInputCol("features")
+  .setOutputCol("indexedFeatures")
+  .setMaxCategories(4) // features with > 4 distinct values are treated as continuous.
+  .fit(data)
+
+// Split the data into training and test sets (30% held out for testing).
+val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+// Train a DecisionTree model.
+val dt = new DecisionTreeClassifier()
+  .setLabelCol("indexedLabel")
+  .setFeaturesCol("indexedFeatures")
+
+// Convert indexed labels back to original labels.
+val labelConverter = new IndexToString()
+  .setInputCol("prediction")
+  .setOutputCol("predictedLabel")
+  .setLabels(labelIndexer.labels)
+
+// Chain indexers and tree in a Pipeline.
+val pipeline = new Pipeline()
+  .setStages(Array(labelIndexer, featureIndexer, dt, labelConverter))
+
+// Train model. This also runs the indexers.
+val model = pipeline.fit(trainingData)
+
+// Make predictions.
+val predictions = model.transform(testData)
