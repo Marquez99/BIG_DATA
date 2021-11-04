19eb4365cd8d9a55cfb42049d9dc3b87e9b1f837
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-03 20:40:57 -0700

    Create Practice1 Decision tree classifier.scala
    Begin of Practice number 1, decision tree

---
+++ Practices/Practice1 Decision tree classifier.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+
+// Load the data stored in LIBSVM format as a DataFrame.
+val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
