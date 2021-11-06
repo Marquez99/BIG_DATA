9cbfa376f97fb58cb8aae3753e0a2913b5d42577
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-05 19:57:04 -0700

    Select (prediction, true label) and compute test error.

--- Practices/Practice3 Random forest classifier.scala
+++ Practices/Practice3 Random forest classifier.scala
@@ -36,3 +36,32 @@ val rf = new RandomForestClassifier()
   .setFeaturesCol("indexedFeatures")
   .setNumTrees(10)
 
+// Convert indexed labels back to original labels.
+val labelConverter = new IndexToString()
+  .setInputCol("prediction")
+  .setOutputCol("predictedLabel")
+  .setLabels(labelIndexer.labels)
+
+// Chain indexers and forest in a Pipeline.
+val pipeline = new Pipeline()
+  .setStages(Array(labelIndexer, featureIndexer, rf, labelConverter))
+
+// Train model. This also runs the indexers.
+val model = pipeline.fit(trainingData)
+
+// Make predictions.
+val predictions = model.transform(testData)
+
+// Select example rows to display.
+predictions.select("predictedLabel", "label", "features").show(5)
+
+// Select (prediction, true label) and compute test error.
+val evaluator = new MulticlassClassificationEvaluator()
+  .setLabelCol("indexedLabel")
+  .setPredictionCol("prediction")
+  .setMetricName("accuracy")
+val accuracy = evaluator.evaluate(predictions)
+println(s"Test Error = ${(1.0 - accuracy)}")
+
+val rfModel = model.stages(2).asInstanceOf[RandomForestClassificationModel]
+println(s"Learned classification forest model:\n ${rfModel.toDebugString}")
