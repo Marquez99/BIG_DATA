80bc82947a88dbca4e87b2f03cda9beca3879a2d
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-05 19:56:34 -0700

    Split the data into training

--- Practices/Practice3 Random forest classifier.scala
+++ Practices/Practice3 Random forest classifier.scala
@@ -13,3 +13,26 @@ import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 // Load and parse the data file, converting it to a DataFrame.
 val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels in index.
+val labelIndexer = new StringIndexer()
+  .setInputCol("label")
+  .setOutputCol("indexedLabel")
+  .fit(data)
+// Automatically identify categorical features, and index them.
+// Set maxCategories so features with > 4 distinct values are treated as continuous.
+val featureIndexer = new VectorIndexer()
+  .setInputCol("features")
+  .setOutputCol("indexedFeatures")
+  .setMaxCategories(4)
+  .fit(data)
+
+// Split the data into training and test sets (30% held out for testing).
+val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
+// Train a RandomForest model.
+val rf = new RandomForestClassifier()
+  .setLabelCol("indexedLabel")
+  .setFeaturesCol("indexedFeatures")
+  .setNumTrees(10)
+
