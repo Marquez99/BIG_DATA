256727746a2975ad3ef11631f444746c650a7f0b
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-05 19:55:21 -0700

    Pratice 3 Load and parde the data file

---
+++ Practices/Practice3 Random forest classifier.scala
@@ -0,0 +1,15 @@
+// PRACTICA 3 Random forest classifier
+
+//MARQUEZ ENRIQUEZ KEVIN EDWIN 17212923
+//LOPEZ MEDRANO JULIO ANTONIO 
+
+
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+
+// Load and parse the data file, converting it to a DataFrame.
+val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
