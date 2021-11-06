719b27946f72c00fcc9d2e22d44fd333c15dde9c
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-11-05 20:01:44 -0700

    Practice 4 Load and create the parse data

---
+++ Practices/Practice 4 Gradient-boosted tree classifier.scala
@@ -0,0 +1,32 @@
+// PRACTICA 4 Gradient-boosted tree classifier
+
+//MARQUEZ ENRIQUEZ KEVIN EDWIN 17212923
+//LOPEZ MEDRANO JULIO ANTONIO 
+
+
+
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{GBTClassificationModel, GBTClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+
+// Load and parse the data file, converting it to a DataFrame.
+val data = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+
+// Index labels, adding metadata to the label column.
+// Fit on whole dataset to include all labels in index.
+val labelIndexer = new StringIndexer()
+  .setInputCol("label")
+  .setOutputCol("indexedLabel")
+  .fit(data)
+// Automatically identify categorical features, and index them.
+// Set maxCategories so features with > 4 distinct values are treated as continuous.
+val featureIndexer = new VectorIndexer()
+  .setInputCol("features")
+  .setOutputCol("indexedFeatures")
+  .setMaxCategories(4)
+  .fit(data)
+
+// Split the data into training and test sets (30% held out for testing).
+val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3))
+
