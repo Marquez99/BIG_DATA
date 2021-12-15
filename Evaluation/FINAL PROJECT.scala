c6375cbc916295b7480974847137bb74b0c2a399
Marquez99 <kevin.marquez17@tectijuana.edu.mx>
2021-12-14 20:47:53 -0800

    Update FINAL PROJECT.scala

--- Evaluation/FINAL PROJECT.scala
+++ Evaluation/FINAL PROJECT.scala
@@ -158,3 +158,88 @@ println(s"DECISION TREE ACCURACY IS = ${accuracy}")
 
 /* LOGISTIC REGRESION */
 
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.feature.{VectorAssembler, StringIndexer, VectorIndexer}
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.SparkSession
+
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
+
+val spark = SparkSession.builder().getOrCreate()
+
+/*Se importan los datos del CSV*/
+val data  = spark.read.option("header","true").option("inferSchema", "true").option("delimiter",";").format("csv").load("bank.csv")
+
+/*Se categoriza las variables de string a valor numérico*/
+val yes = data.withColumn("y",when(col("y").equalTo("yes"),1).otherwise(col("y")))
+val clean = yes.withColumn("y",when(col("y").equalTo("no"),2).otherwise(col("y")))
+val cleanData = clean.withColumn("y",'y.cast("Int"))
+
+/*Creación del Array con los datos seleccionados*/
+val featureCols = Array("age","previous","balance","duration")
+
+/*Creación del Vector en base a los features*/
+val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
+
+/*Transformación a un nuevo DF*/
+val df2 = assembler.transform(cleanData)
+
+/*Rename de columnas*/
+val featuresLabel = df2.withColumnRenamed("y", "label")
+
+/*Selección de index*/
+val dataI = featuresLabel.select("label","features")
+
+/*Creación del Array con los datos de entrenamiento y test*/
+val Array(training, test) = dataI.randomSplit(Array(0.7, 0.3), seed = 12345)
+
+/*Modelo de Regresion*/
+val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+
+val lrModel = lr.fit(training)
+
+var results = lrModel.transform(test)
+
+/*Impresión de los coeficientes e intercepciones*/
+// println(s"Coefficients: \n${lrModel.coefficientMatrix}")
+// println(s"Intercepts: \n${lrModel.interceptVector}")
+
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+
+// Convert test results to RDD using .as and .rdd
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double, Double)].rdd
+
+// Initialize a MulticlassMetrics object
+val metrics = new MulticlassMetrics(predictionAndLabels)
+
+// metrics.accuracy
+
+println(s"LOGISTIC REGRESION ACUURANCY IS= ${metrics.accuracy}")
+
+
+
+
+
+/* final part of project*/ 
+/*MultilayerPerceptron*/
+
+
+// We import the necessary libraries with which we are going to work
+import org.apache.spark.sql.types.DateType
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.StringIndexer
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.feature.IndexToString
+import org.apache.spark.ml.Pipeline
+import org.apache.log4j._
+
+// Remove warnings
+Logger.getLogger("org").setLevel(Level.ERROR)
+
+// We create a spark session and load the CSV data into a datraframe
+val spark = SparkSession.builder().getOrCreate()
+val df = spark.read.option("header","true").option("inferSchema","true").option("delimiter",";").format("csv").load("bank.csv")
